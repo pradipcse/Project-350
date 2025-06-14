48ab1f0821bd51d9a647a4f0e2f26679ffc4d514
Pradip Pashi <pradippashi00@gmail.com>
2025-06-14 23:40:46 +0600

    product serializer is updated

--- Sust_shop/Backend/shop/api/serializers/product.py
+++ Sust_shop/Backend/shop/api/serializers/product.py
@@ -7,11 +7,8 @@ class ProductSerializer(serializers.ModelSerializer):
     number_of_reviews = serializers.IntegerField(read_only=True)
     current_price = serializers.FloatField(read_only=True)
     image = serializers.ImageField(required=False, allow_null=True, use_url=True)
-    
-    # Category shows category name, input expects category name
-    category = serializers.SlugRelatedField(
-        queryset=Category.objects.all(),
-        slug_field='name'
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all()
     )
 
     class Meta:
@@ -20,6 +17,15 @@ class ProductSerializer(serializers.ModelSerializer):
             'id', 'name', 'slug', 'image', 'category',
             'price', 'discount_price', 'current_price', 'stock',
             'description', 'average_rating', 'number_of_reviews',
-            'uploader', 'created_at', 'updated_at'
+            'uploader', 'created_at', '(updated_at'
         ]
-        read_only_fields = ['slug', 'uploader', 'created_at', 'updated_at']
+        read_only_fields = ['slug', 'uploader', 'created_at', '(updated_at']
+
+    def create(self, validated_data):
+        return Product.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
